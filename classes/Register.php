5638915d4a7f3bfc3fd156acf352b47d3b623afe
Psakine <kol2133@yandex.ru>
2020-08-11 20:41:28 +0300

    fix

--- classes/Register.php
+++ classes/Register.php
@@ -4,7 +4,7 @@ class Register
 {
     public static function registerUser($data)
     {
-        if ($data['password'] == $data['password_repeat']) {
+        if ($data['password'] == $data['password_repeat'] or false) {
             unset($data['password_repeat']);
 
             $data['password'] = md5($data['password'] . 'pass');
